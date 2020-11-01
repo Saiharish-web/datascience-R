9a7f45f03ba82d2862d34ca7a3ed06a73ef932a8
Saiharish-web <73770641+Saiharish-web@users.noreply.github.com>
2020-11-01 11:56:53 +0530

    Add files via upload

---
+++ first.R
@@ -0,0 +1,35 @@
+1+1
+23*pi
+3^2
+sqrt(24)
+2*pi*8
+exp(5)
+x=1
+b=2
+c=4
+x*b*c
+x = c(2,4,5,7)
+y = c(2,5,8,9)
+x*y
+y*7
+sqrt(x)
+x<-c(2,3,6,4)
+x[2]
+x[-1]
+x[1]<-3
+x
+x[-1]<-4
+x
+x[x<9]=2
+x
+sai<-data.frame(x=1:3, y = c("a","b","c"))
+sai
+wing=data.frame(name=c("sai","dev","shalom"), sex = c("male","female","trans"),age=c(21,34,45))
+wing
+ind<-data.frame(x=1:3, y = c("a","b","c"))
+ind
+ind[1, ]
+ind[1,2]
+ind[c(1,3),c(2)]
+ind[c(1,3),c(1)]
+ind[c(1,3),c(1,2)]
